1bdd2b09efded33962b90f7b9601a0e6fe60dfe9
e-matsuy <matsuda.yu@e-jan.co.jp>
2020-06-04 15:40:13 +0900

    終端のloggerのmethodに次に連なる処理が設定できないよう修正

--- leaning/chainofresponsibility/DebugLevelLogger.java
+++ leaning/chainofresponsibility/DebugLevelLogger.java
@@ -7,5 +7,31 @@ public class DebugLevelLogger extends AbstractLogger{
 
   public DebugLevelLogger() {
     super(LEVEL, HEADER);
+    this.setNext(null);
+  }
+
+  /**
+   * デバックレベルはすべてのメッセージを出力するので、渡されたものはすべてDEBUGとして出力するようにする。
+   * 実装時このクラスのnextは常にnullである。
+   * @param event 渡されたイベント
+   * @return 必ずtrue
+   */
+  @Override
+  protected boolean isTarget(Event event) {
+    return true;
+  }
+
+  @Override
+  public void log(Event event) {
+    System.out.println(makeLogMessage(event));
+  }
+
+  /**
+   * @param next このloggerの次にrequestを処理するlogger
+   * @throws IllegalAccessError このクラスのsetNextが呼ばれることはない。
+   */
+  @Override
+  public AbstractLogger setNext(AbstractLogger next) {
+    throw new IllegalAccessError();
   }
 }
